7aac01151e124041ebd88e2d375ca555be25af87
Matthieu Falce <matthieu@falce.net>
2023-11-07 18:54:47 +0100

    gitautosync update 70

---
+++ tape_en_cours/.history/hello_world_20231107185437.py
@@ -0,0 +1,16 @@
+print(2 + 2)
+a = 2
+
+a = 2
+b = 2
+match a, b:
+    case 0, 0:
+        print("1")
+    case 1, 1:
+        print("2")
+    case 1, 2:
+        print("3")
+    case _, _:
+        print("toto")
+
+map()
