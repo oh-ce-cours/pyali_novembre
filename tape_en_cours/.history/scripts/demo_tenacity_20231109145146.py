9632d3effd10c8a82cd26129fec0e45d0b651c98
Matthieu Falce <matthieu@falce.net>
2023-11-09 14:51:54 +0100

    gitautosync update 263

---
+++ tape_en_cours/.history/scripts/demo_tenacity_20231109145146.py
@@ -0,0 +1,14 @@
+import random
+
+from tenacity import retry
+
+
+@retry
+def do_something_unreliable():
+    if random.randint(0, 10) > 1:
+        raise IOError("Broken sauce, everything is hosed!!!111one")
+    else:
+        return "Awesome sauce!"
+
+
+print(do_something_unreliable())
